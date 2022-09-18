5d38eeb20e15973351e466352f0dca0cdde0c5a2
km8oz <ouazmir@gmail.com>
2022-09-18 04:00:31 +0300

    v0.1.11

--- src/hooks/fetcher.tsx
+++ src/hooks/fetcher.tsx
@@ -1,30 +1,30 @@
 import { Any } from "@react-spring/types";
 import React, { useContext, useEffect, useState } from "react";
-import { checkemails, SettingsDB } from "../tools";
+import { checkemails } from "../tools";
 export interface exportedFetchedContex { fetched: any[] ,setFetching: React.Dispatch<React.SetStateAction<any[]>> };
 const FetchingContext = React.createContext<exportedFetchedContex>([] as any);
 
-const FetchingProvider = (props:any) => {
-    const [fetching, setFetching] = useState([]);
-    const [fetched, setFetched] = useState<any[]>([]);
-    useEffect(()=>{
-        (async ()=>{
-        let settings = await SettingsDB.get("settings") as any
-        await checkemails({
-            emails: fetching || [""], 
-            sender: settings.sender, 
-            proxyurl: settings.proxyurl, 
-            smtptimout: settings.smtptimout
-        }).then(async (reachable)=>{
-            setFetched(reachable)
-        })
-        })()
-    },[fetching]);
-    const value = {
-        fetched,
-        setFetching
-    }
-    return <FetchingContext.Provider value={value} {...props} />
-}
+// const FetchingProvider = (props:any) => {
+//     const [fetching, setFetching] = useState([]);
+//     const [fetched, setFetched] = useState<any[]>([]);
+//     useEffect(()=>{
+//         (async ()=>{
+//         let settings = await SettingsDB.get("settings") as any
+//         await checkemails({
+//             emails: fetching || [""], 
+//             sender: settings.sender, 
+//             proxyurl: settings.proxyurl, 
+//             smtptimout: settings.smtptimout
+//         }).then(async (reachable)=>{
+//             setFetched(reachable)
+//         })
+//         })()
+//     },[fetching]);
+//     const value = {
+//         fetched,
+//         setFetching
+//     }
+//     return <FetchingContext.Provider value={value} {...props} />
+// }
 
 export const useFetching = () => useContext(FetchingContext)
